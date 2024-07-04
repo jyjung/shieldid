9530b19dc63e41cde53a43d80672f053670973f5
Jung JinYoung <bungker@gmail.com>
2024-07-04 18:04:44 +0900

    update

--- build/lib/shieldid/__init__.py
+++
@@ -1,4 +0,0 @@
-from .device  import device_authorization
-from .app import create_iap_edge_controller_app
-from .jwtutil import get_kid_from_jwt, get_name_with_company_subfix
-from .builder import IapBuilder, EdgeInfoObject
